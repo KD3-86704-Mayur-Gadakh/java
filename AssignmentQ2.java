d8fa4f2afb96a940b4405856d57239cad193d31f
Mayur Gadakh <mayurgadakh07@gmail.com>
2024-10-22 18:33:38 +0530

    Assignment no 6 Q2 Submited

---
+++ AssignmentQ2.java
@@ -0,0 +1,19 @@
+
+//Q2. Write a java program to reverse a String
+
+package com.sunbeam;
+
+public class AssignmentQ2 {
+
+	public static void main(String[] args) {
+
+		
+		String name="mayur";
+		
+		for(int i=name.length()-1;i>=0;i--) {
+			System.out.print(name.charAt(i));
+		}
+
+	}
+
+}
